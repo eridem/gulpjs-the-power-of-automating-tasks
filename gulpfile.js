96c9ac5af8465b74247c150b140a8657cf3fcf25
Miguel Á. Domínguez Coloma <m@eridem.net>
2016-02-23 07:22:52 +0100

    New snippets and examples.

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,38 @@
-﻿
+﻿'use strict';
+
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var del = require('del');
+var notify = require('gulp-notify');
+
+// #-- TUTORIAL: http://doc/05.png --#
+
+gulp.task('op:cleanup', function () {
+  //#region Non interested for the example
+  del(['./dest', './dest2']);
+  //#endregion
+});
+
+gulp.task('op:dummy', function () {
+  gutil.log(gutil.colors.red('Task:'), gutil.colors.yellow('Dummy'));
+});
+
+gulp.task('op:copyFiles', function () {
+ var replacement = require('./replacement.js');
+ 
+   gutil.log(gutil.colors.red('Task:'), gutil.colors.yellow('Copying files'));
+   
+   gulp.src(['./src/**/*.txt', '!./src/**/*.dev'])
+          .pipe(replacement('DUCK', 'GULP'))
+ 
+          // #-- TUTORIAL: What will happen if we call replacement again?
+          //.pipe(replacement('Let', 'It'))
+              
+          .pipe(gulp.dest('./dest'))
+          .pipe(notify('Task: copyFiles')); 
+ 
+});
+
+gulp.task('build', ['op:cleanup', 'op:copyFiles', 'op:dummy'], function () {
+  gutil.log(gutil.colors.red('Task:'), gutil.colors.yellow('Build success'));
+});
--- replacement.js
+++ replacement.js
@@ -1,1 +1,24 @@
-﻿
+﻿var through = require('through2');
+
+var replaceOnFile = function (file, from, to) {
+  if (file.isNull()) {
+
+  } else if (file.isStream()) {
+
+  }
+  else if (file.isBuffer()) {
+   var contentAsString = String(file.contents);
+       
+       var replacementRegex = new RegExp(from, "gi");
+       contentAsString = contentAsString.replace(replacementRegex, to);
+   
+		file.contents = Buffer(contentAsString);
+  }
+  return file;
+};
+
+module.exports = function (from, to) {
+  return through.obj(function (file, encoding, callback) {
+    callback(null, replaceOnFile(file, from, to));
+  });
+};
